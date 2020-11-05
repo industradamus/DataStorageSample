7ec8fa3ed6a06a4fee930776f12f8dd9c3d95cf2
s.buvaka <s.buvaka@tinkoff.ru>
2020-11-05 13:51:13 +0300

    add sharing files

--- app/src/main/java/com/example/datastoragesample/ui/ImageAdapter.kt
+++ app/src/main/java/com/example/datastoragesample/ui/ImageAdapter.kt
@@ -1,9 +1,10 @@
 package com.example.datastoragesample.ui
 
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.paging.PagedListAdapter
+import androidx.core.view.drawToBitmap
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -12,7 +13,7 @@ import com.example.datastoragesample.R
 import com.example.datastoragesample.models.Photo
 import kotlinx.android.synthetic.main.list_item_image.view.*
 
-class ImageAdapter : PagingDataAdapter<Photo, ImageAdapter.ViewHolder>(DiffUtils()) {
+class ImageAdapter(private val onPhotoClickListener: (Bitmap) -> Unit) : PagingDataAdapter<Photo, ImageAdapter.ViewHolder>(DiffUtils()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =
@@ -21,13 +22,15 @@ class ImageAdapter : PagingDataAdapter<Photo, ImageAdapter.ViewHolder>(DiffUtils
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        getItem(position)?.let { holder.bind(it) }
+        getItem(position)?.let { holder.bind(it, onPhotoClickListener) }
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        internal fun bind(photo: Photo) {
+        internal fun bind(photo: Photo, onPhotoClickListener: (Bitmap) -> Unit) {
             with(itemView) {
+                setOnClickListener { onPhotoClickListener(imageContainer.drawToBitmap()) }
+
                 Glide.with(imageContainer)
                     .load(photo.src.medium)
                     .into(imageContainer)
--- app/src/main/java/com/example/datastoragesample/ui/MainActivity.kt
+++ app/src/main/java/com/example/datastoragesample/ui/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.datastoragesample.ui
 
+import android.content.ContentValues
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.paging.Pager
@@ -14,13 +21,15 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private val api: PixelsApi = RemoteProvider().getApi()
     private val dataSource: PhotoPaginationDataSource = PhotoPaginationDataSource(api)
     private val activityDisposable: CompositeDisposable = CompositeDisposable()
-    private val imageAdapter: ImageAdapter = ImageAdapter()
+    private val imageAdapter: ImageAdapter = ImageAdapter(::onPhotoClick)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,6 +68,60 @@ class MainActivity : AppCompatActivity() {
                 }
             )
             .addTo(activityDisposable)
+    }
+
+    // deprecated
+    private fun deprecatedSharing(photo: Bitmap) {
+        // deprecated
+        val bitmapPath = MediaStore.Images.Media.insertImage(contentResolver, photo, "some title", null)
+
+        val bitmapUri = Uri.parse(bitmapPath)
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "image/jpeg"
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bitmapUri)
+        startActivity(Intent.createChooser(shareIntent, "Share Image"))
+    }
+
+    private fun onPhotoClick(photo: Bitmap) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, System.currentTimeMillis().toString())
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                put(MediaStore.MediaColumns.IS_PENDING, 1)
+            }
+        }
+
+        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        try {
+            compressPhoto(uri, photo)
+            sharePhoto(uri)
+        } catch (e: IOException) {
+            if (uri != null) {
+                contentResolver.delete(uri, null, null)
+            }
+            throw IOException(e)
+        } finally {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+                contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0)
+        }
+    }
+
+    private fun compressPhoto(uri: Uri?, photo: Bitmap) =
+        uri?.let {
+            val stream = contentResolver.openOutputStream(uri)
+
+            if (!photo.compress(Bitmap.CompressFormat.JPEG, 100, stream)) {
+                throw IOException("Failed to save bitmap.")
+            }
+
+        } ?: throw IOException("Failed to create new MediaStore record")
+
+    private fun sharePhoto(uri: Uri?) {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "image/jpeg"
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+        startActivity(Intent.createChooser(shareIntent, "Share Image"))
     }
 
     companion object {
